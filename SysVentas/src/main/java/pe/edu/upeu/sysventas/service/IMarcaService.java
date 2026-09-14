ce981d3710a666e5a96d5dacedc32988cdd112ea
maryorycahuapaza <maryorycahuapaza05@gmail.com>
2026-09-14 07:38:51 -0500

    Sesion4 y 5

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/service/IMarcaService.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/service/IMarcaService.java
@@ -2,5 +2,7 @@ package pe.edu.upeu.sysventas.service;
 
 import pe.edu.upeu.sysventas.model.Marca;
 
-public interface IMarcaService extends ICrudGenericoService<Marca, Long> {
+public interface
+
+IMarcaService extends ICrudGenericoService<Marca, Long> {
 }
